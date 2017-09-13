d99ff922a11d191260e6e58bd188cf71f6d4fe56
justin leggett <justin@codingrookie.com>
2017-09-13 11:28:35 -0500

    refactored and cleaned Segment.js

--- src/app/components/Typography/Segment/Segment.js
+++ src/app/components/Typography/Segment/Segment.js
@@ -1,15 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import "./Segment.scss";
 
 const Segment = props => {
-  const SegmentPaddingTop = styled.div`padding-bottom: 1rem;`;
-  const Segment = styled.div`
-    font-size: 2.5rem;
-    background-color: black;
-    padding: 0 10px;
-    text-transform: uppercase;
-  `;
+  const SegmentPaddingBottom = styled.div`padding-bottom: 1rem;`;
   const SegmentWrapper = styled.div`
     width: 90%;
     height: 30px;
@@ -17,12 +10,18 @@ const Segment = props => {
     text-align: center;
     margin: 0 auto;
   `;
+  const Segment = styled.span`
+    font-size: 2.5rem;
+    background-color: black;
+    padding: 0 10px;
+    text-transform: uppercase;
+  `;
   return (
-    <SegmentPaddingTop>
+    <SegmentPaddingBottom>
       <SegmentWrapper>
         <Segment>{props.title}</Segment>
       </SegmentWrapper>
-    </SegmentPaddingTop>
+    </SegmentPaddingBottom>
   );
 };
 
