0bec038d44e69f4db81fae6e2e1c57ebc31e2e88
justin leggett <justin@codingrookie.com>
2017-09-13 08:34:01 -0500

    added a frmed page

---
+++ src/screens/Forms.js
@@ -0,0 +1,8 @@
+import React from "react";
+import DashboardContainer from "../app/components/Dashboard/DashboardContainer";
+
+const Forms = props => {
+  return <div>This is the forms page</div>;
+};
+
+export default Forms;
