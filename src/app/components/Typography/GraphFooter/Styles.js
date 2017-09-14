1f19fe810b0095e1db09c27f68b5f9f6da059feb
justin leggett <justin@codingrookie.com>
2017-09-14 16:20:42 -0500

    converted GraphFooter to styled-component

---
+++ src/app/components/Typography/GraphFooter/Styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Border = styled.div`
+  border-top: 1px solid white;
+  width: 90%;
+  padding-bottom: 0.5em;
+`;
+
+export const UpdateTime = styled.div`
+  font-size: 0.75rem;
+  margin: 0.5rem;
+`;
