5d722cd3b068349e15c55fb433ffd0f7cae4157a
justin leggett <justin@codingrookie.com>
2017-10-24 16:04:50 -0500

    boke down Header component, so it can be used as a stand alone header or Subheader

--- src/app/components/Typography/Header/Header.js
+++ src/app/components/Typography/Header/Header.js
@@ -1,10 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+
+const addTitle = title => {
+  if (title) return <span>{title}</span>;
+};
+
+const addSubTitle = subtitle => {
+  if (subtitle) return <span>{subtitle}</span>;
+};
+
 const Header = props => {
   return (
     <div>
-      <span>{props.title}</span>
-      <span>{props.subtitle}</span>
+      {addTitle(props.title)}
+      {addSubTitle(props.subtitle)}
     </div>
   );
 };
