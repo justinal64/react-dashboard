c1a0de953d93b06d4c585a41649f1969742e9cee
justin leggett <justin@codingrookie.com>
2017-11-06 14:52:02 -0600

    finsihed styling of the first table.

--- src/app/components/Table/Table.js
+++ src/app/components/Table/Table.js
@@ -4,6 +4,12 @@ import { getIcon } from "../Helper/Helper";
 import GraphHeader from "../Typography/GraphHeader/GraphHeader";
 import ProgressBar from "../../components/ProgressBar/ProgressBar";
 import Label from "../../components/Label/Label";
+import { colors } from "../Styles/Styles";
+
+const getTheme = color => {
+  if (color && color.length !== undefined) return colors[color];
+  return "transparent";
+};
 
 class Table extends React.Component {
   constructor(props) {
@@ -22,19 +28,19 @@ class Table extends React.Component {
         </thead>
         <TBody>
           <tr>
-            <TD>ElizabeTD Burke</TD>
+            <TD>Elizabeth Burke</TD>
             <TD>2012/01/01</TD>
             <TD>Member</TD>
             <TD>
-              <Label title="Success" color="Green" />
+              <Label title="Success" color={getTheme("success")} />
             </TD>
           </tr>
           <tr>
-            <TD>TDeodore Winters</TD>
+            <TD>Theodore Winters</TD>
             <TD>2012/01/01</TD>
             <TD>Administartor</TD>
             <TD>
-              <Label title="Banned" color="Red" />
+              <Label title="Banned" color={getTheme("danger")} />
             </TD>
           </tr>
           <tr>
@@ -42,7 +48,23 @@ class Table extends React.Component {
             <TD>2012/01/01</TD>
             <TD>Staff</TD>
             <TD>
-              <Label title="Inactive" color="Grey" />
+              <Label title="Inactive" color={getTheme("secondary")} />
+            </TD>
+          </tr>
+          <tr>
+            <TD>Peter Burke</TD>
+            <TD>2012/01/01</TD>
+            <TD>Staff</TD>
+            <TD>
+              <Label title="Pending" color={getTheme("warning")} />
+            </TD>
+          </tr>
+          <tr>
+            <TD>Neal Caffery</TD>
+            <TD>2012/01/01</TD>
+            <TD>Staff</TD>
+            <TD>
+              <Label title="Active" color={getTheme("success")} />
             </TD>
           </tr>
         </TBody>
