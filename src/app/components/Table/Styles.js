11e35573b931a8b912a7e89c6496cf485e1da063
justin leggett <justin@codingrookie.com>
2017-11-07 09:36:24 -0600

    Completed first Table with Pagination

--- src/app/components/Table/Styles.js
+++ src/app/components/Table/Styles.js
@@ -1,11 +1,20 @@
 import styled from "styled-components";
 
-export const Table = styled.table`width: 100%;`;
+export const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+`;
 
 export const TBody = styled.tbody`background-color: white;`;
 
 export const TableLeftAligned = Table.extend`text-align: left;`;
 
-export const TH = styled.th`font-size: 1.25rem;`;
+export const TH = styled.th`
+  font-size: 1.25rem;
+  border-bottom: 2px solid #e1e6ef;
+`;
 
-export const TD = styled.td`font-size: 1rem;`;
+export const TD = styled.td`
+  font-size: 1rem;
+  border-bottom: 1px solid #e1e6ef;
+`;
