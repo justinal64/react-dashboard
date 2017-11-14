9458a83df50fe6bd75a793ca1c3872dc21288862
justin leggett <justin@codingrookie.com>
2017-11-14 11:50:15 -0600

    added styles.scss to make sure everything is working correctly. I will convert it into a styled component once I have everything working correctly. I also am playing with a test function to work out the logic of adding/removing the active className

--- src/app/components/Tabs/Tabs.js
+++ src/app/components/Tabs/Tabs.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./Styles.scss";
 import styled from "styled-components";
 import { getIcon } from "../Helper/Helper";
 
@@ -21,20 +22,58 @@ const UL = styled.ul`
   }
 `;
 
+const test = e => {
+  e.preventDefault();
+  console.log(e);
+  console.log(this);
+  console.log("Test Working!!");
+};
+
 const Tabs = props => {
   return (
-    <UL>
+    <UL className="tabs">
       <li>
-        <a href="#" className="active">
+        <a href="#" role="button" className="active" onClick={test}>
           Home
         </a>
       </li>
       <li>
-        <a href="#">Profile</a>
+        <a href="#" role="button" onClick={test}>
+          Profile
+        </a>
       </li>
       <li>
-        <a href="#">Messages</a>
+        <a href="#" role="button" onClick={test}>
+          Messages
+        </a>
       </li>
+      <div className="active">
+        1. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </div>
+      <div>
+        2. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </div>
+      <div>
+        3. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </div>
     </UL>
   );
 };
